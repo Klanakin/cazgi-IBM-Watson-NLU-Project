9a1987ab6652eaae6014f89a2a0cb24810b1fe8b
Don Klanakin <don.klanakin@gmail.com>
2021-06-03 20:40:56 +0700

    Successfully Added NLU

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -4,18 +4,15 @@ const app = new express();
 
 dotenv.config();
 function getNLUInstance() {
-    const api_key = process.env.API_KEY;
-    const api_url = process.env.API_URL;
-
     const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
     const { IamAuthenticator } = require("ibm-watson/auth");
 
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: "2020-08-01",
+        version: "2021-03-25",
         authenticator: new IamAuthenticator({
-            apikey: api_key,
+            apikey: process.env.API_KEY,
         }),
-        serviceUrl: api_url,
+        serviceUrl: process.env.API_URL,
     });
     return naturalLanguageUnderstanding;
 }
@@ -30,25 +27,125 @@ app.get("/", (req, res) => {
 });
 
 app.get("/url/emotion", (req, res) => {
+    const inputUrl = req.query.url;
+    const urlAnalyzeParams = {
+        url: inputUrl,
+        features: {
+            entities: {
+                emotion: true,
+                sentiment: false,
+                limit: 2,
+            },
+            keywords: {
+                emotion: true,
+                sentiment: false,
+                limit: 2,
+            },
+        },
+    };
+    const urlEmotion = getNLUInstance()
+        .analyze(urlAnalyzeParams)
+        .then((urlAnalysisResults) => {
+            console.log(JSON.stringify(urlAnalysisResults, null, 2));
+        })
+        .catch((err) => {
+            console.log("error:", err);
+        });
+
+    console.log(urlEmotion);
     return res.send({ happy: "90", sad: "10" });
 });
 
 app.get("/url/sentiment", (req, res) => {
+    const inputUrl = req.query.url;
+    const urlAnalyzeParams = {
+        url: inputUrl,
+        features: {
+            entities: {
+                emotion: false,
+                sentiment: true,
+                limit: 2,
+            },
+            keywords: {
+                emotion: false,
+                sentiment: true,
+                limit: 2,
+            },
+        },
+    };
+    const urlSentiment = getNLUInstance()
+        .analyze(urlAnalyzeParams)
+        .then((urlAnalysisResults) => {
+            console.log(JSON.stringify(urlAnalysisResults, null, 2));
+        })
+        .catch((err) => {
+            console.log("error:", err);
+        });
+
+    console.log(urlSentiment);
     return res.send("url sentiment for " + req.query.url);
 });
 
 app.get("/text/emotion", (req, res) => {
+    const inputText = req.query.text;
+    const textAnalyzeParams = {
+        text: inputText,
+        features: {
+            entities: {
+                emotion: true,
+                sentiment: false,
+                limit: 2,
+            },
+            keywords: {
+                emotion: true,
+                sentiment: false,
+                limit: 2,
+            },
+        },
+    };
+    const textEmotion = getNLUInstance()
+        .analyze(textAnalyzeParams)
+        .then((textAnalysisResults) => {
+            console.log(JSON.stringify(textAnalysisResults, null, 2));
+        })
+        .catch((err) => {
+            console.log("error:", err);
+        });
+
+    console.log(textEmotion);
     return res.send({ happy: "10", sad: "90" });
 });
 
 app.get("/text/sentiment", (req, res) => {
+    const inputText = req.query.text;
+    const textAnalyzeParams = {
+        text: inputText,
+        features: {
+            entities: {
+                emotion: false,
+                sentiment: true,
+                limit: 2,
+            },
+            keywords: {
+                emotion: false,
+                sentiment: true,
+                limit: 2,
+            },
+        },
+    };
+    const textSentiment = getNLUInstance()
+        .analyze(textAnalyzeParams)
+        .then((textAnalysisResults) => {
+            console.log(JSON.stringify(textAnalysisResults, null, 2));
+        })
+        .catch((err) => {
+            console.log("error:", err);
+        });
+
+    console.log(textSentiment);
     return res.send("text sentiment for " + req.query.text);
 });
 
-// let server = app.listen(process.env.PORT || 5555, () => {
-//     console.log("Listening", server.address().port);
-// });
-
-let server = app.listen(8080 || 3000, () => {
+let server = app.listen(process.env.PORT || 8080 || 3000, () => {
     console.log("Listening on port", server.address().port);
 });
