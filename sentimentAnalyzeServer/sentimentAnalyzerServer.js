3c3135fac1afcbed63ab93806bca4e4dbecdd10b
Don Klanakin <don.klanakin@gmail.com>
2021-06-03 13:56:51 +0700

    First commit

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,50 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
 const app = new express();
 
-app.use(express.static('client'))
+dotenv.config();
+function getNLUInstance() {
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
 
-const cors_app = require('cors');
+    const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+    const { IamAuthenticator } = require("ibm-watson/auth");
+
+    const  naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: "2020-08-01";
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+app.use(express.static("client"));
+
+const cors_app = require("cors");
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+app.get("/", (req, res) => {
+    res.render("index.html");
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    return res.send({ happy: "90", sad: "10" });
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    return res.send("url sentiment for " + req.query.url);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    return res.send({ happy: "10", sad: "90" });
 });
 
-let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
+app.get("/text/sentiment", (req, res) => {
+    return res.send("text sentiment for " + req.query.text);
+});
 
+let server = app.listen(3000, () => {
+    console.log("Listening", server.address().port);
+});
