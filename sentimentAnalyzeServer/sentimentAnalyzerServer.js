e95e772b846675d4be0aa316f4f7dcfc11f0ef4b
Don Klanakin <don.klanakin@gmail.com>
2021-06-04 15:36:21 +0700

    Prepare to submist project

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -31,29 +31,23 @@ app.get("/url/emotion", (req, res) => {
     const urlAnalyzeParams = {
         url: inputUrl,
         features: {
-            entities: {
-                emotion: true,
-                sentiment: false,
-                limit: 2,
-            },
-            keywords: {
-                emotion: true,
-                sentiment: false,
-                limit: 2,
-            },
+            emotion: {},
         },
     };
     const urlEmotion = getNLUInstance()
         .analyze(urlAnalyzeParams)
-        .then((urlAnalysisResults) => {
-            console.log(JSON.stringify(urlAnalysisResults, null, 2));
+        .then((analysisResults) => {
+            let urlAnalysisResults = JSON.stringify(
+                analysisResults.result.emotion.document.emotion,
+                null,
+                2
+            );
+            return res.send(urlAnalysisResults);
         })
         .catch((err) => {
             console.log("error:", err);
+            return res.send(err);
         });
-
-    console.log(urlEmotion);
-    return res.send({ happy: "90", sad: "10" });
 });
 
 app.get("/url/sentiment", (req, res) => {
@@ -61,29 +55,20 @@ app.get("/url/sentiment", (req, res) => {
     const urlAnalyzeParams = {
         url: inputUrl,
         features: {
-            entities: {
-                emotion: false,
-                sentiment: true,
-                limit: 2,
-            },
-            keywords: {
-                emotion: false,
-                sentiment: true,
-                limit: 2,
-            },
+            sentiment: {},
         },
     };
+
     const urlSentiment = getNLUInstance()
         .analyze(urlAnalyzeParams)
         .then((urlAnalysisResults) => {
             console.log(JSON.stringify(urlAnalysisResults, null, 2));
+            res.send(urlAnalysisResults.result.sentiment.document.label);
         })
         .catch((err) => {
             console.log("error:", err);
+            return res.send(err);
         });
-
-    console.log(urlSentiment);
-    return res.send("url sentiment for " + req.query.url);
 });
 
 app.get("/text/emotion", (req, res) => {
@@ -91,29 +76,23 @@ app.get("/text/emotion", (req, res) => {
     const textAnalyzeParams = {
         text: inputText,
         features: {
-            entities: {
-                emotion: true,
-                sentiment: false,
-                limit: 2,
-            },
-            keywords: {
-                emotion: true,
-                sentiment: false,
-                limit: 2,
-            },
+            emotion: {},
         },
     };
     const textEmotion = getNLUInstance()
         .analyze(textAnalyzeParams)
-        .then((textAnalysisResults) => {
-            console.log(JSON.stringify(textAnalysisResults, null, 2));
+        .then((analysisResults) => {
+            let textAnalysisResults = JSON.stringify(
+                analysisResults.result.emotion.document.emotion,
+                null,
+                2
+            );
+            return res.send(textAnalysisResults);
         })
         .catch((err) => {
             console.log("error:", err);
+            return res.send(err);
         });
-
-    console.log(textEmotion);
-    return res.send({ happy: "10", sad: "90" });
 });
 
 app.get("/text/sentiment", (req, res) => {
@@ -121,29 +100,20 @@ app.get("/text/sentiment", (req, res) => {
     const textAnalyzeParams = {
         text: inputText,
         features: {
-            entities: {
-                emotion: false,
-                sentiment: true,
-                limit: 2,
-            },
-            keywords: {
-                emotion: false,
-                sentiment: true,
-                limit: 2,
-            },
+            sentiment: {},
         },
     };
     const textSentiment = getNLUInstance()
         .analyze(textAnalyzeParams)
         .then((textAnalysisResults) => {
-            console.log(JSON.stringify(textAnalysisResults, null, 2));
+            return res.send(
+                textAnalysisResults.result.sentiment.document.label
+            );
         })
         .catch((err) => {
             console.log("error:", err);
+            return res.send(err);
         });
-
-    console.log(textSentiment);
-    return res.send("text sentiment for " + req.query.text);
 });
 
 let server = app.listen(process.env.PORT || 8080 || 3000, () => {
